a452d3adb30eb8c96d2c92d14c39fdefbc5b3756
Claudia Borghini <claborghini@gmail.com>
2020-06-26 14:29:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
   i = 0
-  new_numbers[]
   while i < array.length  do
-    new_numbers[i] == array[i] ** 2
+    array[i] = array[i] ** 2
     i += 1
   end
-  new_numbers
+  array
 end
